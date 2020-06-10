73e1fd5ac9040ed67ddea240ebba38f4a94eef4b
Votre Delconte <florian.delconte@protonmail.com>
2020-06-10 21:07:06 +0200

    python script to cut test exemple in a specified resolution

--- test_cut.py
+++ test_cut.py
@@ -7,9 +7,11 @@ import sys, getopt
 import os
 import cv2
 import numpy as np
+from PIL import Image
 
-#dimension of mini image 
-patchdim=300
+
+#dimension of mini image
+patchdim=320
 
 def main(argv):
 
@@ -31,23 +33,51 @@ def main(argv):
             outputDirectory = arg
     imageName=os.path.splitext(os.path.basename(inputImage))[0]
 
+
     print ('Path to image is ', inputImage)
     print ('Output directory is ', outputDirectory)
     print('input image name :',imageName)
-    
-    #relief image
-    img=cv2.imread(inputImage)
-    print('size image :'+str(img.shape))
-    height = img.shape[0]
-    width = img.shape[1]
-    crop_img = np.zeros((patchdim,patchdim,3), np.uint8)
 
-    for row in range(0, height, patchdim):
-        for col in range(0, width, patchdim):
-            print(row,col)
-            crop_img = img[row:row+patchdim, col:col+patchdim]
-            cv2.imwrite(outputDirectory+imageName+"_"+str(row)+"-"+str(col)+".jpg", crop_img) 
-    
+    img = cv2.imread(inputImage,0)
+    img=resizeImg(img)
+    #crop(img)
+    for k,piece in enumerate(crop(img),0):
+        path=os.path.join(outputDirectory,imageName+"-%s.png" % k)
+        cv2.imwrite( path, piece );
+
+
+
+
+#resize img on patchDim multiple
+def resizeImg(img):
+    imHeight = img.shape[0]
+    imWidth = img.shape[1]
+    print("base width : ",imWidth)
+    print("base Height : ",imHeight)
+    unlargeXparam=patchdim-(imWidth%patchdim)
+    unlargeYparam=patchdim-(imHeight%patchdim)
+    img=cv2.resize(img,(imWidth+unlargeXparam,imHeight+unlargeYparam))
+    reimHeight = img.shape[0]
+    reimWidth = img.shape[1]
+    print("resized width : ",reimWidth)
+    print("resized Height : ",reimHeight)
+    return img
+
+#not yet finish
+#add a cyl img on X axis to complete patchDim multiple
+def unlargeImg(img):
+    return img
+
+#crop image in 304*304 thumbnail
+def crop(im):
+    imgHeight = im.shape[0]
+    imgWidth = im.shape[1]
+    for i in range(imgHeight//patchdim):
+        for j in range(imgWidth//patchdim):
+            #print("X : ",j*patchdim,(j+1)*patchdim,"Y : ",i*patchdim,(i+1)*patchdim)
+            yield im[i*patchdim:(i+1)*patchdim,j*patchdim:(j+1)*patchdim]
+
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
